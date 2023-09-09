6f7d03485c17171bccda9f4a31c9e0a31d91a21f
MiljenkoPetrovic <80465450+MiljenkoPetrovic@users.noreply.github.com>
2023-09-10 00:14:00 +0200

    Some fixes

--- src/Components/BestItems/BestItems.js
+++ src/Components/BestItems/BestItems.js
@@ -2,30 +2,19 @@ import Container from 'react-bootstrap/Container';
 import React from "react";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import "./BestItems.css";
+import { StoreItem } from "src\Components\StorePage\StoreItem.tsx"
+import storeItems from "src\Components\data\items.json"
 
 
 export default function BestItems() {
-    return (
-    <Container>
-        <div className='Items'>
-        <Row >
-            <Col>
-                <img
-                    className="item"
-                    src='https://advanti.com/images/category/gpu.png'
-                    width={500}
-                    height={500}
-                />
+    return (       
+    <Row md={2} xs={1} lg={2} className="g-3">
+        {storeItems.map(item => (
+        <Col key={item.id}>
+            <StoreItem {...item} />
             </Col>
-                <img
-                    className="item"
-                    src='https://advanti.com/images/category/gpu.png'
-                    width={500}
-                    height={500}
-                />
-        </Row>
-        </div>
-    </Container>
+        ))}
+    </Row>
+            
     )
 }
