e23bf83e987cfbfc2eaa944a1f2d19acaa60b9d2
MiljenkoPetrovic <80465450+MiljenkoPetrovic@users.noreply.github.com>
2023-09-10 14:53:32 +0200

    Include and libary fixes

--- src/Components/BestItems/BestItems.js
+++ src/Components/BestItems/BestItems.js
@@ -2,12 +2,12 @@ import Container from 'react-bootstrap/Container';
 import React from "react";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { StoreItem } from "src\Components\StorePage\StoreItem.tsx"
-import storeItems from "src\Components\data\items.json"
+import { StoreItem } from "../StorePage/StoreItem.tsx"
+import storeItems from "../data/items.json"
 
 
 export default function BestItems() {
-    return (       
+    return (     
     <Row md={2} xs={1} lg={2} className="g-3">
         {storeItems.map(item => (
         <Col key={item.id}>
@@ -15,6 +15,6 @@ export default function BestItems() {
             </Col>
         ))}
     </Row>
-            
+                
     )
 }
