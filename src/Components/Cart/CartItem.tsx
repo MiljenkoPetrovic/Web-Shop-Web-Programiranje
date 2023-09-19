a068c53a57de3f678e5bc148758972294106fdd8
MiljenkoPetrovic <80465450+MiljenkoPetrovic@users.noreply.github.com>
2023-09-20 01:38:44 +0200

    Changes to the shopping cart and items

--- src/Components/Cart/CartItem.tsx
+++ src/Components/Cart/CartItem.tsx
@@ -1,45 +1,102 @@
-import { Button, Stack } from "react-bootstrap"
-import { useShoppingCart } from "../Context/ShoppingCartContext"
-import storeItems from "../data/items.json"
-import { formatCurrency } from "../utilities/formatCurrency"
+import React from "react";
+import { Button, Stack } from "react-bootstrap";
+import { useShoppingCart } from "../Context/ShoppingCartContext";
+import storeItems from "../data/items.json";
+import { formatCurrency } from "../utilities/formatCurrency";
 
 type CartItemProps = {
-    id: number
-    quantity: number
-  }
-  
-  export function CartItem({ id, quantity }: CartItemProps) {
-    const { removeFromCart } = useShoppingCart()
-    const item = storeItems.find(i => i.id === id)
-    if (item == null) return null
-  
+    id: number;
+    quantity: number;
+    containerClassName?: string;
+    imageClassName?: string;
+    detailsClassName?: string;
+    quantityClassName?: string;
+    priceClassName?: string;
+    removeButtonClassName?: string;
+};
+
+export function CartItem({
+    id,
+    quantity,
+    containerClassName,
+    imageClassName,
+    detailsClassName,
+    quantityClassName,
+    priceClassName,
+    removeButtonClassName,
+}: CartItemProps) {
+    const { removeFromCart, increaseCartQuantity, decreaseCartQuantity } = useShoppingCart();
+    const item = storeItems.find((i) => i.id === id);
+    if (item == null) return null;
+
+    const imageSize = "75px"; // Image size
+
+    const handleRemoveClick = () => {
+        removeFromCart(id);
+    };
+
+    const handleIncreaseClick = () => {
+        increaseCartQuantity(id);
+    };
+
+    const handleDecreaseClick = () => {
+        decreaseCartQuantity(id);
+    };
+
     return (
-      <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
-        <img
-          src={item.imgUrl}
-          style={{ width: "125px", height: "75px", objectFit: "cover" }}
-        />
-        <div className="me-auto">
-          <div>
-            {item.name}{" "}
-            {quantity > 1 && (
-              <span className="text-muted" style={{ fontSize: ".65rem" }}>
-                x{quantity}
-              </span>
-            )}
-          </div>
-          <div className="text-muted" style={{ fontSize: ".75rem" }}>
-            {formatCurrency(item.price)}
-          </div>
+        <div className={`cart-item-container ${containerClassName}`}>
+            <div className="d-flex justify-content-between align-items-center">
+                <div className="d-flex align-items-center">
+                    <img
+                        src={item.imgUrl}
+                        className={`cart-item ${imageClassName}`}
+                        alt={item.name}
+                        style={{ width: imageSize, height: imageSize, objectFit: "cover" }}
+                    />
+                    <div className={`item-details ${detailsClassName}`}>
+                        <div>
+                            <strong>{item.name}</strong>
+                            {quantity > 1 && (
+                                <span className={`quantity-text ${quantityClassName}`}>
+                                    {" x" + quantity}
+                                </span>
+                            )}
+                        </div>
+                        <div className={`text-muted ${priceClassName}`}>
+                            {formatCurrency(item.price * quantity)}
+                        </div>
+                    </div>
+                </div>
+                <div className="d-flex align-items-center">
+                    <Button
+                        variant="outline-danger"
+                        size="sm"
+                        onClick={handleRemoveClick}
+                        className={`remove-button ${removeButtonClassName}`}
+                    >
+                        Remove
+                    </Button>
+                    <div className={`d-flex align-items-center justify-content-between`}>
+                        <Button
+                            variant="primary"
+                            size="sm"
+                            onClick={handleIncreaseClick}
+                            className="mx-2"
+                        >
+                            +1
+                        </Button>
+                        {quantity > 1 && (
+                            <Button
+                                variant="danger"
+                                size="sm"
+                                onClick={handleDecreaseClick}
+                            >
+                                -1
+                            </Button>
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
-        <div> {formatCurrency(item.price * quantity)}</div>
-        <Button
-          variant="outline-danger"
-          size="sm"
-          onClick={() => removeFromCart(item.id)}
-        >
-          &times;
-        </Button>
-      </Stack>
-    )
-  }
+    );
+}
--- src/Components/ShoppingCart/ShoppingCart.tsx
+++ src/Components/ShoppingCart/ShoppingCart.tsx
@@ -1,36 +1,51 @@
-import { Offcanvas, Stack } from "react-bootstrap";
+// ShoppingCart.tsx
+
+import { Button, Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../Context/ShoppingCartContext";
 import { CartItem } from "../Cart/CartItem";
 import { formatCurrency } from "../utilities/formatCurrency";
-import storeItems from "../data/items.json"
+import storeItems from "../data/items.json";
+import './ShoppingCart.css';
 
 type ShoppingCartProps = {
-    isOpen: boolean
-}
+    isOpen: boolean;
+};
 
 export function ShoppingCart({ isOpen }: ShoppingCartProps) {
-    const { closeCart, cartItems } = useShoppingCart()
-    return (
-      <Offcanvas show={isOpen} onHide={closeCart} placement="end">
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Cart</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <Stack gap={3}>
-            {cartItems.map(item => (
-              <CartItem key={item.id} {...item} />
-            ))}
-            <div className="ms-auto fw-bold fs-5">
-              Total{" "}
-              {formatCurrency(
-                cartItems.reduce((total, cartItem) => {
-                  const item = storeItems.find(i => i.id === cartItem.id)
-                  return total + (item?.price || 0) * cartItem.quantity
-                }, 0)
-              )}
-            </div>
-          </Stack>
-        </Offcanvas.Body>
+  const { closeCart, cartItems } = useShoppingCart();
+
+  const handleCheckout = () => {
+      // Implement your checkout logic here
+      // For example, you can redirect the user to a checkout page or display a confirmation message.
+  };
+
+  return (
+      <Offcanvas show={isOpen} onHide={closeCart} className={`shopping-cart-container ${isOpen ? 'open' : ''}`} placement="end" backdrop={true}>
+          <Offcanvas.Header closeButton>
+              <button type="button" className="offcanvas-close-button" aria-label="Close" onClick={closeCart}>
+                  &times;
+              </button>
+              <Offcanvas.Title>Cart</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+              <Stack gap={3}>
+                  {cartItems.map((item) => (
+                      <CartItem key={item.id} {...item} />
+                  ))}
+                  <div className="ms-auto fw-bold fs-5">
+                      Total{" "}
+                      {formatCurrency(
+                          cartItems.reduce((total, cartItem) => {
+                              const item = storeItems.find((i) => i.id === cartItem.id);
+                              return total + (item?.price || 0) * cartItem.quantity;
+                          }, 0)
+                      )}
+                  </div>
+                  <Button variant="primary" size="sm" onClick={handleCheckout}>
+                      Checkout
+                  </Button>
+              </Stack>
+          </Offcanvas.Body>
       </Offcanvas>
-    )
-  }
+  );
+}
